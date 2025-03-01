6a7ee22a3f6c216fcd56bdb82ab031ae1b531200
Administrator <2063622031@qq.com>
2025-03-01 19:28:46 +0800

    修复了一些bug   = =

--- src/main/java/com/lvedy/alternative_twilight/mixin/Hydra/HydraMixin.java
+++ src/main/java/com/lvedy/alternative_twilight/mixin/Hydra/HydraMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.LargeFireball;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,7 +19,6 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import twilightforest.entity.boss.Hydra;
-import twilightforest.init.TFMobEffects;
 
 import java.util.List;
 import java.util.function.Predicate;
