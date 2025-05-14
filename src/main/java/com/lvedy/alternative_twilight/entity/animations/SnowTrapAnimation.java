6e3ecea637412854cc81175f7b445aff9335932f
Administrator <2063622031@qq.com>
2025-05-14 18:53:27 +0800

    update SnowQueen

---
+++ src/main/java/com/lvedy/alternative_twilight/entity/animations/SnowTrapAnimation.java
@@ -0,0 +1,24 @@
+package com.lvedy.alternative_twilight.entity.animations;// Save this class in your mod and generate all required imports
+
+import net.minecraft.client.animation.AnimationChannel;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.Keyframe;
+import net.minecraft.client.animation.KeyframeAnimations;
+
+/**
+ * Made with Blockbench 4.12.3
+ * Exported for Minecraft version 1.19 or later with Mojang mappings
+ * @author Author
+ */
+public class SnowTrapAnimation {
+	public static final AnimationDefinition normal = AnimationDefinition.Builder.withLength(5.0F).looping()
+		.addAnimation("snow_trap", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(5.0F, KeyframeAnimations.degreeVec(0.0F, 360.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(5.0F, KeyframeAnimations.degreeVec(0.0F, 360.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.build();
+}
